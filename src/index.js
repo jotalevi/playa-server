fab4039bf4d46c9436e25697264c73a9116676c2
Eros Talevi <eros.talevi@innova-ti.cl>
2022-02-05 00:16:28 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//import stuff
+require('dotenv').config();
+
+
+//setup PlayaDiscord and start it
+
+//setup PlayaWebPage and start it
+
+//setup PlayaApi and start it
+
+console.log(process.env);
